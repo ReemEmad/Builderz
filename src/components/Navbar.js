21d1e82e4e54e7aab7cfa63a18db2e653f9a8bfb
Reem-Emad96 <reem.emad9611@yahoo.com>
2021-08-16 00:42:35 +0200

    ✨ card

---
+++ src/components/Navbar.js
@@ -0,0 +1,69 @@
+import React from "react"
+import { Navbar, Nav, Container, Button, Badge } from "react-bootstrap"
+import imgSrc from "../assets/icons8-heart-24.png"
+import cartSrc from "../assets/icons8-shopping-cart-50.png"
+import logo from "../assets/logo.jpg"
+export default function AppNavbar() {
+  return (
+    <header>
+      <Navbar expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src={logo}
+              alt="builderz logo"
+              className="d-inline-block align-middle pt-3"
+            ></img>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav ">
+            <Nav className="mx-auto nav-links">
+              <Nav.Link href="#home">
+                <span className="active-link">Home</span>
+              </Nav.Link>
+              <Nav.Link href="#courses">
+                <span className="nav-text">Courses</span>
+              </Nav.Link>
+              <Nav.Link href="#activities">
+                {" "}
+                <span className="nav-text">Activities </span>
+              </Nav.Link>
+              <Nav.Link href="#testimonials">
+                {" "}
+                <span className="nav-text">Testimonials </span>
+              </Nav.Link>
+              <Nav.Link href="#blog">
+                <span className="nav-text">Blog</span>
+              </Nav.Link>
+              <Nav.Link href="#contact">
+                <span className="nav-text">Contact </span>
+              </Nav.Link>
+            </Nav>
+            <Navbar.Text className="mx-4 ">
+              <img
+                alt="like"
+                src={imgSrc}
+                width="21.87px"
+                height="21.87px"
+                className="d-inline-block align-middle"
+              />
+              <span className="cart-icon">
+                <img
+                  alt="Shopping cart"
+                  src={cartSrc}
+                  width="21.87px"
+                  height="21.87px"
+                  className="d-inline-block align-middle navbar-right"
+                />
+                <Badge pill bg="danger" className="align-text-top badge">
+                  2
+                </Badge>
+              </span>
+              <Button className="btn navbar-right">Log in</Button>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
